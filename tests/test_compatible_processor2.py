b963ff1f64a504bc23f8302f62c8a95fa58ce706
revertical <295559861+revertical@users.noreply.github.com>
2026-07-28 12:56:07 +0200

    Update test_compatible_processor2.py

--- tests/test_compatible_processor2.py
+++ tests/test_compatible_processor2.py
@@ -1,6 +1,8 @@
-import pytchat
+import json
+from pytchat.parser.live import Parser
 from pytchat.processors.compatible.processor import CompatibleProcessor
 
+parser = Parser(is_replay=False)
 
 root_keys = ('kind', 'etag', 'nextPageToken', 'pollingIntervalMillis', 'pageInfo', 'items')
 item_keys = ('kind', 'etag', 'id', 'snippet', 'authorDetails')
@@ -8,17 +10,24 @@ snippet_keys = ('type', 'liveChatId', 'authorChannelId', 'publishedAt', 'hasDisp
 author_details_keys = ('channelId', 'channelUrl', 'displayName', 'profileImageUrl', 'isVerified', 'isChatOwner', 'isChatSponsor', 'isChatModerator')
 
 def test_compatible_processor():
-    stream = pytchat.create("Hj-wnLIYKjw", seektime = 6000, processor=CompatibleProcessor())
-    while stream.is_alive():
-        chat = stream.get()
-        for key in chat.keys():
-            assert key in root_keys
-        for key in chat["items"][0].keys():
-            assert key in item_keys
-        for key in chat["items"][0]["snippet"].keys():
-            assert key in snippet_keys
-        for key in chat["items"][0]["authorDetails"].keys():
-            assert key in author_details_keys
-        break
+    processor = CompatibleProcessor()
 
-    
+    with open("tests/testdata/compatible/textmessage.json", mode='r', encoding='utf-8') as f:
+        _json = f.read()
+
+    _, chatdata = parser.parse(parser.get_contents(json.loads(_json))[0])
+    data = {
+        "video_id": "",
+        "timeout": 7,
+        "chatdata": chatdata
+    }
+    ret = processor.process([data])
+
+    for key in ret.keys():
+        assert key in root_keys
+    for key in ret["items"][0].keys():
+        assert key in item_keys
+    for key in ret["items"][0]["snippet"].keys():
+        assert key in snippet_keys
+    for key in ret["items"][0]["authorDetails"].keys():
+        assert key in author_details_keys
